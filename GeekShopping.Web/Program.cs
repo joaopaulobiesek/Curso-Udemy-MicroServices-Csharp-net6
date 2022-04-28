697669452ab6157f452b8ed46c6ccf2706b05a08
joaopaulobiesek <52977973+joaopaulobiesek@users.noreply.github.com>
2022-04-28 17:45:27 -0300

    Session 9 Finalized

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,6 +1,16 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var UrlProductAPI = builder.Configuration["ServiceUrls:ProductAPI"];
+
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+    c.BaseAddress = new Uri(UrlProductAPI)
+    );
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
