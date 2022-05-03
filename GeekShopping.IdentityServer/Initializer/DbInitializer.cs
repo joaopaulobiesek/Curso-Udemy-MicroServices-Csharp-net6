474a12761ffce7c769fc2fc46f5296406897b287
joaopaulobiesek <52977973+joaopaulobiesek@users.noreply.github.com>
2022-05-03 17:50:51 -0300

    Added DbInitializer

---
+++ GeekShopping.IdentityServer/Initializer/DbInitializer.cs
@@ -0,0 +1,47 @@
+﻿using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace GeekShopping.IdentityServer.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly MySQLContext _context;
+        private readonly UserManager<ApplicationUser> _user;
+        private readonly RoleManager<ApplicationUser> _role;
+
+        public DbInitializer(RoleManager<ApplicationUser> role, UserManager<ApplicationUser> user, MySQLContext context)
+        {
+            _context = context;
+            _user = user;
+            _role = role;
+        }
+
+        public void Initializer()
+        {
+            if (_role.FindByNameAsync(IdentityConfiguration.Admin).Result != null) return;
+            _role.CreateAsync(new IdentityRole(IdentityConfiguration.Admin)).GetAwaiter().GetResult();
+            _role.CreateAsync(new IdentityRole(IdentityConfiguration.Client)).GetAwaiter().GetResult();
+
+            ApplicationUser admin = new ApplicationUser()
+            {
+                UserName = "joaopaulobiesek",
+                Email = "joaopaulobiesek@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "+55 (77) 99914-6515",
+                FirstName = "João Paulo",
+                LastName = "Biesek de Oliveira"
+            };
+
+            _user.CreateAsync(admin, "*Joao123").GetAwaiter().GetResult();
+            _user.AddToRoleAsync(admin, IdentityConfiguration.Admin).GetAwaiter().GetResult();
+            var adminClaims = _user.AddClaimAsync(admin, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, $"{admin.FirstName} {admin.LastName}")
+            }).Result;
+        }
+    }
+}
--- GeekShopping.IdentityServer/Program.cs
+++ GeekShopping.IdentityServer/Program.cs
@@ -1,6 +1,38 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+    options.UseMySql(
+        connection,
+        new MySqlServerVersion(
+            new Version(8, 0, 29))));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var builders = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+.AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+.AddInMemoryClients(IdentityConfiguration.clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+builders.AddDeveloperSigningCredential();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -10,9 +42,14 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
